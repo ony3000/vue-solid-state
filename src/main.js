a4e0798094da006a900020d79fa8cbd517e9f586
Hyeonjong <nianelo4@gmail.com>
2018-10-09 01:52:56 +0900

    Add entry point component

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+import Vue from 'vue/dist/vue.runtime.esm.js';
+import SiteApp from './site-app.vue';
+
+new Vue({
+    el: '#page-wrapper',
+    components: {
+        SiteApp,
+    },
+    render(h) {
+        return h(SiteApp);
+    },
+});
+
 window.addEventListener('load', function () {
     setTimeout(function () {
         document.querySelector('body').classList.remove('is-preload');
---
+++ src/site-app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div id="page-wrapper"></div>
+</template>
+
+<script>
+export default {
+    name: 'site-app',
+};
+</script>
