927d9704077a85caed9ab6d40cfe568778e9fbb4
Hyeonjong <nianelo4@gmail.com>
2018-10-13 13:49:12 +0900

    Prune away some scripts of template source

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -11,15 +11,6 @@
 		$header = $('#header'),
 		$banner = $('#banner');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
-
 	// Play initial animations on page load.
 		$window.on('load', function() {
 			window.setTimeout(function() {
