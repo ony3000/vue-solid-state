0e6093dbb2c481020e93ed979e1bfd2c892d312f
Hyeonjong <nianelo4@gmail.com>
2018-10-14 18:15:39 +0900

    Use Vuex syntax for state handlers in menu element

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,45 @@ const store = new Vuex.Store({
     state: {
         isMenuLocked: false,
     },
+    mutations: {
+        locking(state) {
+            state.isMenuLocked = true;
+        },
+        unlocking(state) {
+            state.isMenuLocked = false;
+        },
+    },
+    actions: {
+        lockMenu(context) {
+            return new Promise((resolve, reject) => {
+                if (!context.state.isMenuLocked) {
+                    context.commit('locking');
+                    setTimeout(() => {
+                        context.commit('unlocking');
+                    }, 350);
+                    resolve();
+                }
+            });
+        },
+        showMenu(context) {
+            context.dispatch('lockMenu')
+                .then(() => {
+                    document.querySelector('body').classList.add('is-menu-visible');
+                })
+        },
+        hideMenu(context) {
+            context.dispatch('lockMenu')
+                .then(() => {
+                    document.querySelector('body').classList.remove('is-menu-visible');
+                })
+        },
+        toggleMenu(context) {
+            context.dispatch('lockMenu')
+                .then(() => {
+                    document.querySelector('body').classList.toggle('is-menu-visible');
+                })
+        },
+    },
 });
 
 export default store;
