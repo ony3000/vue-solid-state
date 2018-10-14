5e8c4209dcbf406d7cf33bc9db47aa9bfd935547
Hyeonjong <nianelo4@gmail.com>
2018-10-14 18:49:38 +0900

    Simplify markup of repeated navigation links

--- src/components/site-menu.vue
+++ src/components/site-menu.vue
@@ -3,11 +3,9 @@
         <div class="inner">
             <h2>Menu</h2>
             <ul class="links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/generic">Generic</a></li>
-                <li><a href="/elements">Elements</a></li>
-                <li><a href="#">Log In</a></li>
-                <li><a href="#">Sign Up</a></li>
+                <li v-for="(item, index) in linkItems" :key="index">
+                    <a :href="item.href">{{ item.text }}</a>
+                </li>
             </ul>
             <a href="#" class="close">Close</a>
         </div>
@@ -17,5 +15,31 @@
 <script>
 export default {
     name: 'site-menu',
+    data() {
+        return {
+            linkItems: [
+                {
+                    href: '/',
+                    text: 'Home',
+                },
+                {
+                    href: '/generic',
+                    text: 'Generic',
+                },
+                {
+                    href: '/elements',
+                    text: 'Elements',
+                },
+                {
+                    href: '#',
+                    text: 'Log In',
+                },
+                {
+                    href: '#',
+                    text: 'Sign Up',
+                },
+            ],
+        };
+    },
 };
 </script>
